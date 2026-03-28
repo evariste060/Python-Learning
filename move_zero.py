0fb484e926d1bec5987acd5cf0f364e9ffb310bd
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-03-28 19:12:03 +0200

    Add the function that move zeros without changing the order

---
+++ move_zero.py
@@ -0,0 +1,9 @@
+def moveZeroes(nums):
+    left = 0  
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+    return nums
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
